73659e68da56c2c893dd16c27e1cb4709047844b
Stephy-Duran <stephany.duran@globant.com>
2024-09-01 21:23:40 -0500

    [ADD] project creation and connection between the framework and device

---
+++ mobilePractice/src/test/java/com/globant/academy/tests/NavigationManuBar.java
@@ -0,0 +1,15 @@
+package com.globant.academy.tests;
+
+import com.globant.academy.screens.HomeScreen;
+import com.globant.academy.utils.test.BaseTest;
+import org.testng.annotations.Test;
+
+public class NavigationManuBar extends BaseTest {
+
+    
+    @Test
+    public void NavigationOnTheBottomMenuBar(){
+        HomeScreen homeScreen = openHomeScreen();
+        
+    }
+}
